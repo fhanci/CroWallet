757fc8c99bb8f844080fd436b2e54d2efc6a93e2
AybarsKansu <aybars810@gmail.com>
2025-08-05 10:29:45 +0300

    fixed debt issue

--- api/target/generated-sources/annotations/com/example/api/mapper/DebtMapperImpl.java
+++ api/target/generated-sources/annotations/com/example/api/mapper/DebtMapperImpl.java
@@ -12,7 +12,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-08-04T13:48:02+0300",
+    date = "2025-08-05T10:22:14+0300",
     comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.42.50.v20250729-0351, environment: Java 21.0.8 (Eclipse Adoptium)"
 )
 public class DebtMapperImpl implements DebtMapper {
